2f13b8602b56284a1c675b800f969ae70bb20a2e
Chris Beams <chris@beams.io>
2014-07-06 10:36:35 +0200

    Add webhook-aware serve script

--- serve.sh
+++ serve.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-#
-# Serve the site locally on port 4000. Assumes that `jekyll` has been
-# installed per instructions at http://jekyllrb.com/docs/installation
-#
-jekyll serve --watch --drafts
+
+HOOKPORT=12345
+SERVEPORT=80
+
+while true; do
+    echo starting jekyll on port $SERVEPORT...
+    jekyll serve --port $SERVEPORT &
+    JEKYLLPID=$!
+    echo jekyll started with pid $JEKYLLPID.
+    echo listening for webhook notifications on port $HOOKPORT.
+    nc -l $HOOKPORT
+    echo got webhook request. updating git repo...
+    git pull
+    echo git repo updated. killing jekyll.
+    kill -9 $!
+done
