816daa0e0bfd15e7fb52fd2b4bfe293d3b0a7ea6
Chris Beams <chris@beams.io>
2014-07-06 15:16:02 +0200

    Update serve.sh to survive force push / rebase

--- serve.sh
+++ serve.sh
@@ -6,7 +6,9 @@ SERVEPORT=4000
 cd $(dirname $0)
 
 echo updating git repo...
-/usr/local/bin/git pull
+/usr/local/bin/git stash # just to be safe
+/usr/local/bin/git fetch origin
+/usr/local/bin/git reset --hard origin/gh-pages
 
 while true; do
     echo starting jekyll on port $SERVEPORT...
@@ -16,7 +18,9 @@ while true; do
     echo listening for webhook notifications on port $HOOKPORT.
     echo -e "HTTP/1.1 202 Accepted\r\nContent-Type: text/html\r\n\r\n" | nc -l $HOOKPORT
     echo got webhook request. updating git repo...
-    /usr/local/bin/git pull
+    /usr/local/bin/git stash # just to be safe
+    /usr/local/bin/git fetch origin
+    /usr/local/bin/git reset --hard origin/gh-pages
     echo git repo updated. killing jekyll.
     kill -9 $!
 done
