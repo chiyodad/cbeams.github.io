2fb35b7d893a93302ae9b8b01f5b805e7ad7523e
Chris Beams <chris@beams.io>
2014-07-06 15:05:24 +0200

    Add debug logs to launch daemon config
    Also, fully-qualify the path to git as it was otherwise not being found
    when running as root.
    
    See "Debugging launchd jobs" at https://developer.apple.com/library/mac/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CreatingLaunchdJobs.html#//apple_ref/doc/uid/10000172i-SW7-BCIEDDBJ

--- serve.sh
+++ serve.sh
@@ -6,7 +6,7 @@ SERVEPORT=4000
 cd $(dirname $0)
 
 echo updating git repo...
-git pull
+/usr/local/bin/git pull
 
 while true; do
     echo starting jekyll on port $SERVEPORT...
@@ -16,7 +16,7 @@ while true; do
     echo listening for webhook notifications on port $HOOKPORT.
     echo -e "HTTP/1.1 202 Accepted\r\nContent-Type: text/html\r\n\r\n" | nc -l $HOOKPORT
     echo got webhook request. updating git repo...
-    git pull
+    /usr/local/bin/git pull
     echo git repo updated. killing jekyll.
     kill -9 $!
 done
