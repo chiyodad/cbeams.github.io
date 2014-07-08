ee5b8adf214061136d4629a8568f5535d1d2a5ee
Chris Beams <chris@beams.io>
2014-07-08 16:16:18 +0200

    Add Jekyll config overrides for running locally

--- servelocal.sh
+++ servelocal.sh
@@ -3,4 +3,4 @@
 # Serve the site locally on port 4000. Assumes that `jekyll` has been
 # installed per instructions at http://jekyllrb.com/docs/installation
 #
-jekyll serve --watch --drafts
+jekyll serve --watch --drafts --config _config.yml,_config-local.yml
